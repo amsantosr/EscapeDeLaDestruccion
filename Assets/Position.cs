af9ef72d0d4b57aacced49b0d9b1366e92b6b4ea
Abraham Santos <amsantosr@gmail.com>
2014-01-25 17:28:47 -0500

    Add falling spheres :D

--- Assets/Position.cs
+++ Assets/Position.cs
@@ -5,45 +5,50 @@ public class Position : MonoBehaviour {
 	private System.Random rnd;
 	private int difficultyLimit;
 	// Use this for initialization
-
+	
 	public Position() : base(){
 		rnd = new System.Random ();
 		difficultyLimit = 2;
 	}
-
+	
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		int randomNumber = rnd.Next (100);
-
-
+		
+		
 		if (randomNumber < difficultyLimit) {
-			for (int i=0; i<2; i++) {
-				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
-				cube.AddComponent<Rigidbody> ();
-				cube.transform.position = new Vector3 (betweenRange(this.transform.position.x,0.5),this.transform.position.y+4.0f,this.transform.position.z);
-
+			GameObject gameObject;
+			if (rnd.Next() % 2 == 0)
+			{
+				gameObject = GameObject.CreatePrimitive (PrimitiveType.Cube);
 			}
+			else
+			{
+				gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			}
+			gameObject.AddComponent<Rigidbody> ();
+			gameObject.transform.position = new Vector3 (betweenRange(this.transform.position.x,0.5),this.transform.position.y+4.0f,this.transform.position.z);
 		}
-
+		
 	}
-
+	
 	private float betweenRange(double number, double range){
 		bool sign;
 		double numToSum;
 		if (rnd.Next (0, 100) < 50) {
-				sign = false;
+			sign = false;
 		} else {
-				sign = true;
+			sign = true;
 		}
 		numToSum = rnd.NextDouble ()*range;
 		if (!sign) {
 			numToSum = numToSum * -1;
 		}
-
+		
 		return (float)(numToSum + number);
 	}
 }
