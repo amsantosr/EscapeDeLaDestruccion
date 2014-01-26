2ab4aee1543baf783f2091111bb9ec6a2c2f544f
Raul Gallegos <raul.ogh@gmail.com>
2014-01-25 19:02:21 -0500

    Prefab of passage added

---
+++ Assets/WallCreation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCreation : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	void Start () {
+		//GameObject myPassage = (GameObject)Instantiate(Resources.Load("Passage"), new Vector3 (, 5, 5), Quaternion.identity);
+		//myPassage.AddComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
