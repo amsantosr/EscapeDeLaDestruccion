1d46858ece8227f663316df45d14d054a9de5905
Raul Gallegos <raul.ogh@gmail.com>
2014-01-25 20:47:46 -0500

    just a gameover comment

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameOver : MonoBehaviour {
 	
 	void OnControllerColliderHit(ControllerColliderHit hit) {
+		// ONLY CONTROLS COLLISIONS IF THE CONTROLLER IS "MOVING"
 		if (hit.gameObject.name == "AUTO" && hit.gameObject.transform.position.y > this.transform.position.y) {
 			Application.LoadLevel ("GameOver");
 		}
